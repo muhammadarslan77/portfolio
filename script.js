7d3134b756c7ccfa48d7b1aecbee3b6a3c64d08c
Muhammad Arslan <81929745+muhammadarslan77@users.noreply.github.com>
2025-01-22 23:08:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,8 @@ document
       }
     );
   });
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show');
+});
